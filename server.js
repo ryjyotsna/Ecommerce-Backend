63f21ef1302ef9ec3538fe36e21dd5c42b5d7070
Jyotsna Ray <ryjyotsna@gmail.com>
2026-04-15 00:51:21 +0530

    added all routes

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = express();
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoute from "./routes/auth.js";
+import categoryRoute from "./routes/category.js";
+import productRoute from "./routes/product.js";
+import cartRoute from "./routes/cart.js";
+import orderRoute from "./routes/order.js";
+import invoiceRoute from "./routes/invoice.js";
+
+
+
 dotenv.config({
   path: "./.env",
 });
@@ -17,6 +25,11 @@ app.get("/health", (req, res) => {
 
 //Routes
 app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/category", categoryRoute);
+app.use("/api/v1/product", productRoute);
+app.use("/api/v1/cart", cartRoute);
+app.use("/api/v1/order", orderRoute);
+app.use("/api/v1/invoice", invoiceRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running`);
